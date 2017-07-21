74fc4311718bdb6cc990d593a239895c0f42f55a
CarlosAlenquer <30277857+CarlosAlenquer@users.noreply.github.com>
2017-07-21 03:45:14 +0100

    Update
    Completei a imagem de registo e login, organizei o codigo do css com comentarios, meti o slideshow automatico de 4 em 4 s. muda de slide e manual. Falta so o contacta-nos e footer e por o site responsive em outros sitios

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,4 +1,5 @@
 var slideIndex = 1;
+var slideIndexCycle = 0;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -12,15 +13,22 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+}
+
+function showSlidesCycle() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndexCycle++;
+    if (slideIndexCycle> slides.length) {slideIndexCycle = 1} 
+    slides[slideIndexCycle-1].style.display = "block"; 
+    setTimeout(showSlidesCycle, 4000); // Muda a imagem a cada 4 segundos
 }
