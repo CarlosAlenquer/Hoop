1b976861b572c7fa23c3b2e128442c9ef1dd052c
CarlosAlenquer <30277857+CarlosAlenquer@users.noreply.github.com>
2017-07-31 02:16:59 +0100

    mais coisas para verificar se preencheu os campos com JS

--- js/checkform.js
+++ js/checkform.js
@@ -1,6 +1,6 @@
 function checkForm() {
     var flag1, flag2, flag3 = 0;
-
+    
     var nome = document.getElementById("name");
     if (nome.checkValidity() == false) {
         flag1 = 1;
@@ -21,7 +21,7 @@ function checkForm() {
     }
 
     if(flag1 == 0 && flag2 == 1 && flag3 == 0) {
-        lert("Preencha o seu email!");
+        alert("Preencha o seu email!");
     }
 
     if(flag1 == 0 && flag2 == 0 && flag3 == 1) {
@@ -40,8 +40,15 @@ function checkForm() {
         alert("Preencha o seu email e o seu texto!");
     }
 
-     if(flag1 == 1 && flag2 == 0 && flag3 == 1) {
+    if(flag1 == 1 && flag2 == 0 && flag3 == 1) {
         alert("Preencha o seu nome e o seu texto!");
     }
 
+}
+
+function checkForm2() {
+    var emailNews = document.getElementById("emailNews2");
+    if (emailNews.checkValidity() == false) {
+        alert("Preencha o seu email para receber a newsletter!");
+    } 
 }
